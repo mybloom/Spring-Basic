8c9a86b2f6942e0e44ff28c5fda0e95d20edeaf7
mybloom <baejungeun11@gmail.com>
2022-03-02 23:27:21 +0900

    AppConfig를 application전체구성을 빠르게 파악할 수 있도록 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -7,13 +8,23 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+//method 명을 보는 순간 역할을 알 수가 있게 된다.
+//application전체 구성을 빠르게 파악할 수 있다.
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+	private MemoryMemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+
+	public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
+
+	public DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();
+	}
 }
