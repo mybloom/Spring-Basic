bb5c279d084740f53fae551b38d0bf43c1ac1324
mybloom <baejungeun11@gmail.com>
2022-03-02 23:10:18 +0900

    DI구현 첫번째 - AppConfig를 이용해 의존 객체 생성 및 주입

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,11 +9,11 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-//    public MemberService memberService() {
-//        return new MemberServiceImpl(new MemoryMemberRepository());
-//    }
-//
-//    public OrderService orderService() {
-//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
-//    }
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
 }
