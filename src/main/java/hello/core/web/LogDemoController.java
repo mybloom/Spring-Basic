451b4e21665d364eeb7d0d8c7418326b65efcdc8
mybloom <baejungeun11@gmail.com>
2022-05-03 16:31:32 +0900

    feat: ObjectProvider를 이용해 request스코프 객체 생성을 지연

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger; //생성시 의존관계 주입이 일어나는데 고객 요청이 없기 때문에 의존관계 주입을 할 수 없다.
+	private final ObjectProvider<MyLogger> myLoggerProvider; //DL이 주입된다.
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {//자바에서 제공하는 표준 서블릿 규약.
 		String requestUrl = request.getRequestURL().toString();
+		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.setRequestURL(requestUrl);
 
 		myLogger.log("controller test");
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerProvider;
 
 	public void logic(String id) {
+		MyLogger myLogger = myLoggerProvider.getObject();
 		System.out.println("service id = " + id);
 	}
 }
